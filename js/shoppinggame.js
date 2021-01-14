93f6b6ce3102ad23d453a3a3887a51b2dd2394be
Egli Malaj <egli.malaj@communardo.com>
2021-01-14 14:28:07 +0100

    Task 2-5

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -19,11 +19,23 @@ const player = {
     }
 };
 // Define the Product class - write the Constructor function for Product class here
-const 
+function Product(id,name,price,expiryDate){
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.expiryDate = expiryDate
+}
 
 
 // Complete the dateDiff function
 const dateDiff = (date1, date2) => {};
+
+const dateDiff = (date1,date2)=>{
+    let diff = Math.abs(date2.getTime() - date1.getTime());
+    let diffDay = Math.ceil(diff / (1000*3600*24));
+
+    return diffDay;
+}
 
 // Here, use Object.defineProperty to create property - daysToExpire
 
